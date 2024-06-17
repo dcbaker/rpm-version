3b4c29b9e4b7cd3b4b48b9e12e64ba7182080a7c
Dylan Baker <dylan@pnwbakers.com>
2024-06-17 14:23:37 -0700

    Revert "Check for unsupported and use it"
    This reverts commit 7fa19f5873abf3e7fe9287925d5d9a7c91992f5f.
    
    Unreachable is probably the wrong solution here, I'd really prefer to
    have std::expected, but I also don't want to require C++23. I could use
    tl::expected, but that drags in dependencies which we currently don't
    have.

--- src/private.hpp
+++ src/private.hpp
@@ -1,5 +1,5 @@
-// SPDX-License-Identifier: Apache-2.0 OR GPL-2.0
-// Copyright © 2021-2024 Intel Corporation
+// SPDX-license-identifier: Apache-2.0 OR GPL-2.0
+// Copyright © 2021-2022 Intel Corporation
 
 #pragma once
 
@@ -11,16 +11,4 @@ namespace RPMVersion {
 
 std::string to_string(const Operator op);
 
-void assert_fn(bool expr, const char * msg);
-
 } // namespace RPMVersion
-
-#if RPM_VERSION_HAS_UNREACHABLE
-#define RPM_VERSION_UNREACHABLE(msg)                                                               \
-    do {                                                                                           \
-        RPMVersion::assert_fn(false, msg);                                                         \
-        __builtin_unreachable();                                                                   \
-    } while (false)
-#else
-#define RPM_VERSION_UNREACHABLE(msg) RPMVersion::assert_fn(false);
-#endif
